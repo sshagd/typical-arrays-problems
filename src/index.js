6f6cea7690d7768b5640d26c3c90e8b469bc094a
sshagd <70946387+sshagd@users.noreply.github.com>
2020-09-30 12:04:56 +0300

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if((typeof array == "undefined") || (array.length < 1)){
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    if((typeof array == "undefined") || (array.length < 1)){
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if((typeof array == "undefined") || (array.length < 1)){
+        return 0;
+    } else
+  return array.reduce((a, b) => (a + b)) / array.length;
+};
